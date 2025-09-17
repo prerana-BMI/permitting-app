ffbac2eb561b074d702b6c787727db019ac46efe
prerana-BMI <prrasam@burnsmcd.com>
2025-09-17 19:51:10 +0530

    updation as per deployment error

--- src/assets/environments/environment.prod.ts
+++ src/assets/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.yourdomain.com/api' // live API URL
+    apiUrl: 'permitappapiservice-h5egcsewe3djcgan.southcentralus-01.azurewebsites.net' // live API URL
   };
