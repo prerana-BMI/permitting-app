56576b90974e56ced8df901073e4ebf04580c7e0
prerana-BMI <prrasam@burnsmcd.com>
2025-09-22 19:38:32 +0530

    prod config

--- src/assets/environments/environment.prod.ts
+++ src/assets/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://permitappapiservice-h5egcsewe3djcgan.southcentralus-01.azurewebsites.net' // live API URL
+    BaseUrl: 'https://permitappapiservice-h5egcsewe3djcgan.southcentralus-01.azurewebsites.net' // live API URL
   };
