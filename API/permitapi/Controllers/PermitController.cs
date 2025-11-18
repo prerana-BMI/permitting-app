f4d1682038aa95d67b3d9501ab396ad164c08059
prerana-BMI <prrasam@burnsmcd.com>
2025-11-18 18:18:47 +0530

    deploy ui issues on

--- API/permitapi/Controllers/PermitController.cs
+++ API/permitapi/Controllers/PermitController.cs
@@ -855,6 +855,7 @@ namespace permitapi.Controllers
                     RegulatoryAgencyName = item.GetValueOrDefault("Regulatory Agency"),
                     RegulatoryAgencyId = Convert.ToInt32(item.GetValueOrDefault("Regulatory Agency ID")),
                     TypeOfProject = item.GetValueOrDefault("Type Of Project"),
+                    FromBulkUpload = true
                 };
 
                 var PermitMasterDetailsObj = new PermitMasterDetail()
